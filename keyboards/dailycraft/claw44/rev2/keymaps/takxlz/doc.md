789b9c4ded3ebf628247689467b55870e020ad45
takxlz <tak.root.yk1012@gmail.com>
2025-11-30 19:37:20 +0900

    Adjust keymap

--- keyboards/dailycraft/claw44/rev2/keymaps/takxlz/doc.md
+++ keyboards/dailycraft/claw44/rev2/keymaps/takxlz/doc.md
@@ -3,3 +3,122 @@ make dailycraft/claw44:takxlz
 
 ### 書き込み
 make dailycraft/claw44:takxlz:flash
+
+# QMKファームウェア構築方法の比較  
+foostan / Yowkees / dailycraft の違い
+
+---
+
+## 1. リポジトリの位置づけの違い
+
+| リポジトリ | 位置づけ・目的 | 特徴 |
+|------------|----------------|------|
+| foostan/kbd_firmware | QMK/Vial を内包するラッパー型ファームウェア管理リポジトリ | サブモジュール管理、makeラッパー、crkbd特化 |
+| Yowkees/keyball | 標準QMK環境にKeyball定義を追加して使う方式 | 標準QMKフローでビルド可能 |
+| dailycraft/qmk_firmware | QMK本体をDailyCraft向けにforkした専用版QMK | 専用QMKとして単体使用可能 |
+
+---
+
+## 2. ビルド環境構築の流れの違い
+
+### ■ foostan/kbd_firmware（ラッパー型）
+
+```
+git clone https://github.com/foostan/kbd_firmware
+cd kbd_firmware
+make git-submodule
+```
+
+ビルド例：
+
+```
+kb=crkbd kr=rev4_1/standard km=via make qmk-compile
+make vial-qmk-compile kb=crkbd km=vial
+```
+
+特徴：
+- QMK/Vial をサブモジュールとして内包
+- makeコマンドが独自ラッパー形式
+- リポジトリ内ですべて完結
+
+---
+
+### ■ Yowkees/keyball（標準QMK拡張型）
+
+```
+git clone https://github.com/qmk/qmk_firmware.git
+git clone https://github.com/Yowkees/keyball.git
+mv keyball/qmk_firmware/keyboards/keyball/ qmk_firmware/keyboards/
+```
+
+ビルド例：
+
+```
+cd qmk_firmware
+make keyball44:default
+qmk compile -kb keyball44 -km default
+```
+
+特徴：
+- 公式QMKをそのまま使用
+- キーボード定義のみ追加して使う
+- 標準QMKの構造や流儀を理解するのに向く
+
+---
+
+### ■ dailycraft/qmk_firmware（専用QMK型）
+
+```
+git clone https://github.com/dailycraftkeyboard/qmk_firmware.git
+cd qmk_firmware
+git checkout dailycraft-0.22
+```
+
+ビルド例：
+
+```
+make dailycraft/claw44/rev2:default
+make dailycraft/claw44/rev2:via
+```
+
+特徴：
+- QMK本体そのものがDailyCraft専用にカスタマイズ済み
+- Claw44 / Wings42 など複数モデルを標準で内包
+- 標準QMK形式のままビルド可能（ラッパー不要）
+
+---
+
+## 3. ビルド方式の特徴比較
+
+| 観点 | foostan | Yowkees | dailycraft |
+|------|---------|---------|------------|
+| QMK本体 | サブモジュール内包 | 公式QMKを別途clone | QMK本体fork構成 |
+| 定義追加 | 内包済み | 手動コピー | 内包済み |
+| ビルド方式 | 独自makeラッパー | 標準QMK | 標準QMK |
+| VIA対応 | 強い | 要設定 | 強い |
+| 学習性 | △ | ◎ | ○ |
+| 実用性 | ○ | △ | ◎ |
+
+---
+
+## 4. 用途別のおすすめ
+
+| 利用目的 | 適したリポジトリ |
+|----------|------------------|
+| 標準QMKの学習・理解 | Yowkees |
+| Claw44やWings42専用で使いたい | dailycraft |
+| Corne用で完結させたい | foostan |
+| VIAで柔軟に運用したい | dailycraft or foostan |
+| 自分でQMKを改造したい | dailycraft |
+
+---
+
+## 5. 一言まとめ
+
+| リポジトリ | 一言でいうと |
+|------------|--------------|
+| foostan | Corne専用のファームウェア管理セット |
+| Yowkees | 標準QMKにKeyball定義を足して使う方式 |
+| dailycraft | DailyCraft向けに調整された専用QMK本体 |
+
+---
--- keyboards/dailycraft/claw44/rev2/keymaps/takxlz/keymap.c
+++ keyboards/dailycraft/claw44/rev2/keymaps/takxlz/keymap.c
@@ -17,12 +17,13 @@ enum layer_number {
 enum custom_keycodes {
     TK_FWRD = SAFE_RANGE, // 進む
     TK_BACK,              // 戻る
-    TK_MCTL,              // ミッションコントロール
-    TK_SCLN,              // セミコロン、コロン
-    TK_PLAS,              // プラス、アスタリスク
     TK_SLEP,              // スリープ
-    TK_EISU,              // 英数
-    TK_KANA,              // かな
+    TK_REST,              // リセット
+    // TK_MCTL,              // ミッションコントロール
+    // TK_SCLN,              // セミコロン、コロン
+    // TK_PLAS,              // プラス、アスタリスク
+    // TK_EISU,              // 英数
+    // TK_KANA,              // かな
 };
 
 #ifdef TAP_DANCE_ENABLE
@@ -30,6 +31,8 @@ enum custom_keycodes {
 enum {
     TD_EN = 0,
     TD_JP,
+    TD_F10,
+    TD_F2,
 };
 
 // TD種別定義
@@ -65,6 +68,7 @@ int cur_dance(tap_dance_state_t *state) {
 
 /************************************************************ タップダンス ************************************************************/
 #ifdef TAP_DANCE_ENABLE
+/*
 // TD_EN
 void td_en_finished(tap_dance_state_t *state, void *user_data) {
     td_state = cur_dance(state);
@@ -75,12 +79,7 @@ void td_en_finished(tap_dance_state_t *state, void *user_data) {
             register_code(KC_LGUI);
         }
     } else {
-        if (detected_host_os() == OS_WINDOWS) {
-            tap_code(KC_LNG2);
-            tap_code(KC_F22);
-        } else {
-            tap_code(KC_LNG2);
-        }
+        tap_code(KC_LNG2);
     }
 }
 void td_en_reset(tap_dance_state_t *state, void *user_data) {
@@ -98,27 +97,81 @@ void td_en_reset(tap_dance_state_t *state, void *user_data) {
 void td_jp_finished(tap_dance_state_t *state, void *user_data) {
     td_state = cur_dance(state);
     if (td_state == SINGLE_HOLD) {
-        register_code(KC_LGUI);
-    } else {
         if (detected_host_os() == OS_WINDOWS) {
-            tap_code(KC_LNG1);
-            tap_code(KC_F21);
+            register_code(KC_LGUI);
         } else {
-            tap_code(KC_LNG1);
+            register_code(KC_LCTL);
         }
+    } else {
+        tap_code(KC_LNG1);
     }
 }
 void td_jp_reset(tap_dance_state_t *state, void *user_data) {
     if (td_state == SINGLE_HOLD) {
-        unregister_code(KC_LGUI);
+        if (detected_host_os() == OS_WINDOWS) {
+            unregister_code(KC_LGUI);
+        } else {
+            unregister_code(KC_LCTL);
+        }
+    }
+    td_state = 0;
+}
+*/
+
+// TD_F10
+void td_f10_finished(tap_dance_state_t *state, void *user_data) {
+    td_state = cur_dance(state);
+    if (td_state == SINGLE_HOLD) {
+        if (detected_host_os() == OS_WINDOWS) {
+            register_code(KC_LCTL);
+        } else {
+            register_code(KC_LGUI);
+        }
+    } else {
+        tap_code(KC_F10);
+    }
+}
+void td_f10_reset(tap_dance_state_t *state, void *user_data) {
+    if (td_state == SINGLE_HOLD) {
+        if (detected_host_os() == OS_WINDOWS) {
+            unregister_code(KC_LCTL);
+        } else {
+            unregister_code(KC_LGUI);
+        }
+    }
+    td_state = 0;
+}
+
+// TD_F2
+void td_f2_finished(tap_dance_state_t *state, void *user_data) {
+    td_state = cur_dance(state);
+    if (td_state == SINGLE_HOLD) {
+        if (detected_host_os() == OS_WINDOWS) {
+            register_code(KC_LGUI);
+        } else {
+            register_code(KC_LCTL);
+        }
+    } else {
+        tap_code(KC_F2);
+    }
+}
+void td_f2_reset(tap_dance_state_t *state, void *user_data) {
+    if (td_state == SINGLE_HOLD) {
+        if (detected_host_os() == OS_WINDOWS) {
+            unregister_code(KC_LGUI);
+        } else {
+            unregister_code(KC_LCTL);
+        }
     }
     td_state = 0;
 }
 
 // TD割り当て
 tap_dance_action_t tap_dance_actions[] = {
-    [TD_EN] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_en_finished, td_en_reset),
-    [TD_JP] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_jp_finished, td_jp_reset),
+    // [TD_EN] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_en_finished, td_en_reset),
+    // [TD_JP] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_jp_finished, td_jp_reset),
+    [TD_F10] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_f10_finished, td_f10_reset),
+    [TD_F2] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_f2_finished, td_f2_reset),
 };
 #endif
 
@@ -128,38 +181,49 @@ tap_dance_action_t tap_dance_actions[] = {
 #define KC_2_ENT LT(_2, KC_ENT)
 
 // lctl_t
-// #define KC_C_JA LCTL_T(KC_LNG1)
-#define KC_C_ESC LCTL_T(KC_ESC)
-#define KC_C_G LCTL_T(KC_G)
-#define KC_C_H LCTL_T(KC_H)
+#define KC_C_ENT LCTL_T(KC_ENT)
+#define KC_C_MINS LCTL_T(KC_MINS)
+#define KC_C_F LCTL_T(KC_F)
+#define KC_C_J LCTL_T(KC_J)
 
 // lgui_t
-// #define KC_G_EN LGUI_T(KC_LNG2)
-#define KC_G_F LGUI_T(KC_F)
-#define KC_G_J LGUI_T(KC_J)
+#define KC_G_G LGUI_T(KC_G)
+#define KC_G_H LGUI_T(KC_H)
+#define KC_G_ESC LGUI_T(KC_ESC)
+#define KC_G_LBRC LGUI_T(KC_LBRC)
 
 // lsft_t
-#define KC_S_TAB LSFT_T(KC_TAB)
+#define KC_S_ENT LSFT_T(KC_ENT)
+#define KC_S_EN LSFT_T(KC_LNG2)
+#define KC_S_JP LSFT_T(KC_LNG1)
+#define KC_S_BS LSFT_T(KC_BSPC)
+#define KC_S_DEL LSFT_T(KC_DEL)
+#define KC_S_ALT LSFT_T(KC_LALT)
 
 // lalt_t
 #define KC_A_D LALT_T(KC_D)
 #define KC_A_K LALT_T(KC_K)
+#define KC_A_TAB LALT_T(KC_TAB)
 
 // cmd+shift
 #define KC_GS_S SCMD_T(KC_S)
 #define KC_GS_L SCMD_T(KC_L)
 
+// ctrl+shift
+#define KC_CS_A MT(MOD_LCTL | MOD_LSFT, KC_A)
+#define KC_CS_SCLN MT(MOD_LCTL | MOD_LSFT, KC_SCLN)
+
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    [_0] = LAYOUT(
     // .-----------------------------------------------------------------------------------.                  .-----------------------------------------------------------------------------------.
-        KC_RBRC      , KC_Q        , KC_W        , KC_E        , KC_R        , KC_T        ,                   KC_Y         , KC_U        , KC_I        , KC_O        , KC_P        , KC_NUHS     ,
+        KC_G_ESC     , KC_Q        , KC_W        , KC_E        , KC_R        , KC_T        ,                   KC_Y         , KC_U        , KC_I        , KC_O        , KC_P        , KC_G_LBRC   ,
     // .-------------+-------------+-------------+-------------+-------------+-------------.                  .-------------+-------------+-------------+-------------+-------------+-------------.
-        KC_C_ESC     , KC_A        , KC_GS_S     , KC_A_D      , KC_G_F      , KC_C_G      , XXXXXXX, XXXXXXX, KC_C_H       , KC_G_J      , KC_A_K      , KC_GS_L     , KC_SCLN     , KC_MINS     ,
+        KC_C_ENT     , KC_CS_A     , KC_GS_S     , KC_A_D      , KC_C_F      , KC_G_G      , XXXXXXX, XXXXXXX, KC_G_H       , KC_C_J      , KC_A_K      , KC_GS_L     , KC_CS_SCLN  , KC_C_MINS   ,
     // .-------------+-------------+-------------+-------------+-------------+-------------.                  .-------------+-------------+-------------+-------------+-------------+-------------.
-        KC_LEFT      , KC_Z        , KC_X        , KC_C        , KC_V        , KC_B        , XXXXXXX, XXXXXXX, KC_N         , KC_M        , KC_COMM     , KC_DOT      , KC_SLSH     , KC_RGHT     ,
+        KC_S_BS      , KC_Z        , KC_X        , KC_C        , KC_V        , KC_B        , XXXXXXX, XXXXXXX, KC_N         , KC_M        , KC_COMM     , KC_DOT      , KC_SLSH     , KC_S_ALT   ,
     // .-------------+-------------+-------------+-------------+-------------+-------------.                  .-------------+-------------+-------------+-------------+-------------+-------------.
-                                     KC_S_TAB    , TD(TD_EN)   , KC_1_SPC    , KC_BSPC     ,                   KC_BSPC      , KC_2_ENT    , TD(TD_JP)   , KC_S_TAB
+                                     TD(TD_F10)  , KC_S_EN     , KC_1_SPC    , KC_A_TAB    ,                   KC_BSPC      , KC_2_ENT    , KC_S_JP     , TD(TD_F2)
     // ,                            -------------------------------------------------------.                  .-------------------------------------------------------.
    ),
 
@@ -167,11 +231,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // .-----------------------------------------------------------------------------------.                  .-----------------------------------------------------------------------------------.
         _______      , KC_F1       , KC_F2       , KC_F3       , KC_F4       , KC_F5       ,                   KC_SLSH      , S(KC_MINS)  , S(KC_QUOT)  , S(KC_SCLN)  , S(KC_5)     , KC_PGUP     ,
     // .-------------+-------------+-------------+-------------+-------------+-------------.                  .-------------+-------------+-------------+-------------+-------------+-------------.
-        KC_EQL       , KC_1        , KC_2        , KC_3        , KC_4        , KC_5        , XXXXXXX, XXXXXXX, KC_6         , KC_7        , KC_8        , KC_9        , KC_0        , _______     ,
+        S(KC_INT1)   , KC_1        , KC_2        , KC_3        , KC_4        , KC_5        , XXXXXXX, XXXXXXX, KC_6         , KC_7        , KC_8        , KC_9        , KC_0        , _______     ,
     // .-------------+-------------+-------------+-------------+-------------+-------------.                  .-------------+-------------+-------------+-------------+-------------+-------------.
-        _______      , KC_F6       , KC_F7       , KC_F8       , KC_F9       , KC_F10      , XXXXXXX, XXXXXXX, KC_F11       , KC_F12      , _______     , _______     , S(KC_4)     , KC_PGDN     ,
+        KC_S_DEL     , KC_F6       , KC_F7       , KC_F8       , KC_F9       , KC_F10      , XXXXXXX, XXXXXXX, _______      , _______      , _______    , _______     , S(KC_4)     , KC_PGDN     ,
     // .-------------+-------------+-------------+-------------+-------------+-------------.                  .-------------+-------------+-------------+-------------+-------------+-------------.
-                                     _______     , _______     , _______     , KC_DEL      ,                   KC_DEL       , _______     , _______     , _______
+                                     _______     , _______     , _______     , _______     ,                   _______       , _______     , _______    , _______
     // ,                            -------------------------------------------------------.                  .-------------------------------------------------------.
    ),
 
@@ -181,28 +245,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // .-------------+-------------+-------------+-------------+-------------+-------------.                  .-------------+-------------+-------------+-------------+-------------+-------------.
         KC_EQL       , S(KC_3)     , S(KC_LBRC)  , S(KC_2)     , S(KC_7)     , S(KC_EQL)   , XXXXXXX, XXXXXXX, KC_LEFT      , KC_DOWN     , KC_UP       , KC_RGHT     , KC_QUOT     , S(KC_INT1)  ,
     // .-------------+-------------+-------------+-------------+-------------+-------------.                  .-------------+-------------+-------------+-------------+-------------+-------------.
-        _______      , KC_LBRC     , S(KC_COMM)  , S(KC_RBRC)  , KC_RBRC     , S(KC_8)     , XXXXXXX, XXXXXXX, S(KC_9)      , KC_NUHS     , S(KC_NUHS)  , S(KC_DOT)   , S(KC_4)     , KC_END      ,
+        KC_S_DEL     , KC_LBRC     , S(KC_COMM)  , KC_RBRC     , S(KC_8)     , S(KC_RBRC)  , XXXXXXX, XXXXXXX, S(KC_NUHS)   , S(KC_9)     , KC_NUHS     , S(KC_DOT)   , S(KC_4)     , KC_END      ,
     // .-------------+-------------+-------------+-------------+-------------+-------------.                  .-------------+-------------+-------------+-------------+-------------+-------------.
-                                     _______     , _______     , _______     , KC_DEL      ,                   KC_DEL       , _______     , _______     , _______
+                                     _______     , _______     , _______     , _______     ,                   _______      , _______     , _______     , _______
     // ,                            -------------------------------------------------------.                  .-------------------------------------------------------.
    ),
 
    [_3] = LAYOUT(
     // .-----------------------------------------------------------------------------------.                  .-----------------------------------------------------------------------------------.
-        KC_PWR       , _______     , _______     , _______     , _______     , _______     ,                   _______      , _______     , _______     , _______     , _______     , _______     ,
+        _______      , _______     , _______     , _______     , _______     , _______     ,                   _______      , _______     , _______     , _______     , KC_PSCR     , _______     ,
     // .-------------+-------------+-------------+-------------+-------------+-------------.                  .-------------+-------------+-------------+-------------+-------------+-------------.
-        _______      , _______     , KC_GS_S     , KC_PSCR     , _______     , _______     , XXXXXXX, XXXXXXX, KC_MS_LEFT   , KC_MS_DOWN  , KC_MS_UP    , KC_MS_RIGHT , _______     , _______     ,
+        _______      , _______     , _______     , _______     , TK_BACK     , TK_FWRD     , XXXXXXX, XXXXXXX, KC_MS_LEFT   , KC_MS_DOWN  , KC_MS_UP    , KC_MS_RIGHT , _______     , _______     ,
     // .-------------+-------------+-------------+-------------+-------------+-------------.                  .-------------+-------------+-------------+-------------+-------------+-------------.
-        _______      , _______     , _______     , _______     , _______     , _______     , XXXXXXX, XXXXXXX, KC_BTN3      , KC_BTN1     , KC_BTN2     , _______     , _______     , _______     ,
+        _______      , _______     , _______     , _______     , KC_F11      , KC_F12      , XXXXXXX, XXXXXXX, KC_BTN3      , KC_BTN1     , KC_BTN2     , _______     , _______     , _______     ,
     // .-------------+-------------+-------------+-------------+-------------+-------------.                  .-------------+-------------+-------------+-------------+-------------+-------------.
-                                     _______     , _______     , _______     , _______     ,                   _______      , _______     , _______     , _______
+                                     _______     , _______     , _______     , _______     ,                   _______      , _______     , TK_REST     , TK_SLEP
     // ,                            -------------------------------------------------------.                  .-------------------------------------------------------.
    ),
 };
 // clang-format on
 
 /************************************************************ カスタムキー ************************************************************/
-/*
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   // 進む
   if (keycode == TK_FWRD && record -> event.pressed) {
@@ -230,6 +293,21 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
     return false;
   }
+  // スリープ
+  if (keycode == TK_SLEP && record -> event.pressed) {
+    tap_code16(LALT(LGUI(KC_EJCT)));
+    return false;
+  }
+  // リセット
+  if (keycode == TK_REST && record -> event.pressed) {
+    clear_keyboard();
+    // unregister_code(KC_LGUI);
+    // unregister_code(KC_LCTL);
+    // unregister_code(KC_LSFT);
+    // unregister_code(KC_LALT);
+    return false;
+  }
+  /*
   // ミッションコントロール
   if (keycode == TK_MCTL && record -> event.pressed) {
     if (detected_host_os() == OS_WINDOWS) {
@@ -265,16 +343,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
     return false;
   }
-  // スリープ
-  if (keycode == TK_SLEP && record -> event.pressed) {
-    tap_code16(LALT(LGUI(KC_EJCT)));
-    // register_code(KC_LALT);
-    // register_code(KC_LGUI);
-    // tap_code(KC_EJCT);
-    // unregister_code(KC_LGUI);
-    // unregister_code(KC_LALT);
-    return false;
-  }
   // セミコロン、コロン
   if (keycode == TK_SCLN && record -> event.pressed) {
     bool shift = get_mods() & MOD_MASK_SHIFT;
@@ -300,19 +368,32 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
     return false;
   }
+  */
   return true;
 };
-*/
 
 /************************************************************ その他 ************************************************************/
 // キーごとにTAPPING_TERMを調整する
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
+        // レイヤー0：左手中段
+        case KC_C_ENT:
+        case KC_CS_A:
         case KC_GS_S:
-        case KC_GS_L:
         case KC_A_D:
+        case KC_C_F:
+        case KC_G_G:
+        // レイヤー0：右手中段
+        case KC_C_MINS:
+        case KC_CS_SCLN:
+        case KC_GS_L:
         case KC_A_K:
-            return TAPPING_LAYER_TERM;
+        case KC_C_J:
+        case KC_G_H:
+            return TAPPING_TERM_LONG;
+        case KC_S_EN:
+        case KC_S_JP:
+            return TAPPING_TERM_SHORT;
         default:
             return TAPPING_TERM;
     }
